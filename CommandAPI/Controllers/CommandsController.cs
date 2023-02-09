1438e7b8d35525d7f70fddd581a5acdd7ccf0d3b
UbunKor <korban.torsun@gmail.com>
2023-02-08 19:38:04 -0800

    initial commit

---
+++ CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CommandsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new[] { "this", "is", "hard", "coded" };
+    }
+}
