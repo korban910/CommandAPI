2a49e21ca6aa15773aa098d970c4b76d7f578297
korban910 <korban.torsun@gmail.com>
2023-02-22 20:39:19 -0800

    Added user-secrets method

--- CommandAPI/Program.cs
+++ CommandAPI/Program.cs
@@ -1,12 +1,17 @@
 using CommandAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectBuilder = new NpgsqlConnectionStringBuilder();
+connectBuilder.ConnectionString = builder.Configuration.GetConnectionString("PostgresSqlConnection");
+connectBuilder.Username = builder.Configuration["UserID"];
+connectBuilder.Password = builder.Configuration["Password"];
 builder.Services.AddDbContext<CommandContext>(opt =>
 {
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresSqlConnection"));
+    opt.UseNpgsql(connectBuilder.ConnectionString);
     // opt.UseSql(builder.Configuration.GetConnectionString("MySqlConnection"));
 });
 builder.Services.AddControllers();
--- CommandAPI/obj/Debug/net6.0/CommandAPI.AssemblyInfo.cs
+++ CommandAPI/obj/Debug/net6.0/CommandAPI.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f1c464ee-261d-44c8-8b77-7ca2f9c4d7f8")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
